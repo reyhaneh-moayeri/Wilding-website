d9b44b4423d820715f7a4ed24e6a757de6930dbc
reyhaneh <reyhanehrm333@gmail.com>
2021-10-22 00:49:31 +0330

    footer and comments section was added + added fontface

---
+++ up/js/slider.js
@@ -0,0 +1,12 @@
+const sliderContainer = document.querySelector(".comments__slider");
+const arrowRight = document.querySelector(".comments__slider--arrows__right");
+const arrowLeft = document.querySelector(".comments__slider--arrows__left");
+const arrowsContainer = document.querySelector(".comments__slider--arrows");
+const slide = document.querySelectorAll(".comments__slider--slide");
+console.log(slide);
+
+// arrowRight.addEventListener("click", function () {
+//   slide.forEach((s, index) => {
+//     s.style.transform = `translateX(${66}%)`;
+//   });
+// });
